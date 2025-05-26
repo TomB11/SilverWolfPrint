c99a7bd7d8aa64d6bc43d8bc5652f15bc23cfb7a
Tomas Baranek <tbaranek82@gmail.com>
2025-05-26 02:22:19 +0200

    Added style changes

--- swp/src/app/pages/home-page/home-page.component.ts
+++ swp/src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BillboardComponent } from "../../components/billboard/billboard.component";
+import { CardComponent } from "../../components/card/card.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [BillboardComponent],
+  imports: [BillboardComponent, CardComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
